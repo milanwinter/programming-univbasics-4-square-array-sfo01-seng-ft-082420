ff998dd1b2b2dd3fa03067a1b9ad7c83160950ba
Milan Winter <milanwinter8@gmail.com>
2020-07-29 06:55:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array array
   new_array = []
-  index = 0
-  while index < array.length do
-    new_array.push array[index] ** 2
+  i = 0
+  while i < array.length do
+    new_array.push array[i] ** 2
     i += 1
   end
 end
